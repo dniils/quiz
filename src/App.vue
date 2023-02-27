eb98514e2d40aa438d53435400a94f2b1817af35
10 <tochka098@gmail.com>
2023-02-28 01:37:21 +0400

    layout and rendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <WelcomeMessage />
+  <QuizComponent />
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import QuizComponent from "@/components/QuizComponent.vue";
+import WelcomeMessage from "@/components/WelcomeMessage.vue";
 </script>
 
 <style lang="scss">
@@ -15,6 +15,6 @@ import HelloWorld from "./components/HelloWorld.vue";
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
   // color: #2c3e50;
-  // margin-top: 60px;
+  margin-top: 60px;
 }
 </style>
