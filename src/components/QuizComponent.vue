81e4e4d33f5715ce766d3953a203fa7aff5fb318
10 <tochka098@gmail.com>
2023-03-02 16:50:45 +0400

    added comparison of answers with correct answers

--- src/components/QuizComponent.vue
+++ src/components/QuizComponent.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="quiz-component">
     <form class="quiz" @submit.prevent="submitEventHandler">
-      <div class="quiz__task" v-for="task in quizData" v-bind:key="task.id">
+      <div
+        class="quiz__task"
+        v-for="(task, questionNumber) in quizData"
+        v-bind:key="task.id"
+      >
         <div class="quiz__content">
           <div class="quiz__question">{{ task.question }}</div>
           <div class="quiz__hint">{{ task.inputHint }}</div>
           <div class="quiz__options">
             <!-- input text answer -->
             <input
+              @input="getSelectedOption(task.id, $event)"
               class="quiz__input_text"
               type="text"
               v-if="task.type === 'text'"
@@ -24,13 +29,12 @@
                 :key="option"
               >
                 <input
-                  @change="getSelectedOption"
+                  @change="getSelectedOption(task.id, $event)"
                   class="quiz__input_radio"
                   type="radio"
                   :name="task.id.toString()"
-                  :id="task.id + '_' + option"
+                  :id="task.id.toString()"
                   :value="option"
-                  :v-model="answer"
                 />
                 <label
                   class="quiz__radiobuttons-label"
@@ -38,7 +42,6 @@
                   >{{ option }}</label
                 >
               </div>
-              {{ answer }}
             </div>
 
             <!-- multiple-choice question (checkboxes) -->
@@ -49,10 +52,11 @@
                 :key="option"
               >
                 <input
+                  @change="getSelectedOption(task.id, $event)"
                   class="quiz__input_checkbox"
                   type="checkbox"
                   :name="task.id.toString()"
-                  :id="option"
+                  :id="task.id.toString()"
                   :value="option"
                 />
                 <label class="quiz__checkbox-label" :for="option">
@@ -66,7 +70,7 @@
         <img class="quiz__image" v-if="task.image" :src="task.image" alt="" />
         <div class="quiz__question-number">
           <!-- Make some el counter not dependent on questions id's (questions order will be randomized) -->
-          <div>{{ task.id + 1 }}</div>
+          <div>{{ questionNumber + 1 }}</div>
         </div>
       </div>
       <main-button>✨ Submit ✨</main-button>
@@ -79,36 +83,89 @@
 
 // todos:
 // [x] make a function that randomizes elements order in Array
-// [x] randomize answer options order ()
+// [ ] randomize answer options order ()
 // [ ] make it ☝️ optional
+
 // [ ] edit data first (arrays' elements order), then display it
 
-// [ ] randomize questions order
+// [x] randomize questions order
 // [ ] make it ☝️ optional
 
-// [ ] store selected answers and compare them with correct ones
+// [x] get selected answers and compare them with correct ones
+// [ ] get questions id's with incorrect answer
+// [ ] calculate points according to the questions weight
 
 import { ref } from "vue";
 import questionsData from "@/questions.json";
 import MainButton from "@/components/MainButton.vue";
-import { tsAnyKeyword } from "@babel/types";
 
-const quizData = questionsData; //create TS Type to check data structure and types (?)
+const quizData = mixArrayElements(questionsData); //create TS Type to check data structure and types (?)
+const answers = ref({});
+let checkboxAnswers = ref([]);
 
 function mixArrayElements<T>(arr: T[]): T[] {
   return arr.sort((a, b) => 0.5 - Math.random());
 }
 
-const answer = ref();
-// const answers = ref([]);
-// function getSelectedOption(e) {
-//   answers.value.push(e.target._value);
-//   // console.log(answers.value);
-//   console.log(e.target);
-// }
+// Get all the selected/written answers into the 'answers' object by task id
+function getSelectedOption(taskId, e) {
+  if (e.target.type === "radio") {
+    answers.value[taskId] = e.target._value;
+  }
+
+  if (e.target.type === "checkbox") {
+    if (e.target.checked) checkboxAnswers.value.push(e.target.value);
+    if (!e.target.checked) {
+      checkboxAnswers.value = checkboxAnswers.value.filter(
+        (el) => el !== e.target.value
+      );
+    }
+
+    checkboxAnswers.value.filter((el) => el !== e.target.checked);
+    answers.value[taskId] = checkboxAnswers.value;
+  }
+
+  if (e.target.type === "text") {
+    answers.value[taskId] = e.target.value;
+  }
+}
+
+// get questions ids & corresponding correct answers from data
+function getCorrectAnswers() {
+  let correctAnswersWithIds = {};
+  quizData.map((task) => {
+    correctAnswersWithIds[task.id] = task.correct;
+  });
+
+  return correctAnswersWithIds;
+}
+getCorrectAnswers();
+
+function compareTwoArrays(a: Array<string>, b: Array<string>) {
+  return a.length === b.length && a.sort().every((el, i) => el === b.sort()[i]);
+}
+
+// compare correct answers with the given ones (answers.value ~ getCorrectAnswers())
+function compareAnswers(given, correct) {
+  let results = [];
+  for (let id = 0; id < Object.keys(given).length; id++) {
+    if (typeof Object.entries(given)[id][1] === "string") {
+      results.push(
+        given[Object.keys(given)[id]].toLowerCase() ===
+          correct[Object.keys(correct)[id]].toLowerCase()
+      );
+    } else if (typeof Object.entries(given)[id][1] === "object") {
+      const givenAnswersArray = given[Object.keys(given)[id]];
+      const correctAnswersArray = correct[Object.keys(correct)[id]];
+      results.push(compareTwoArrays(givenAnswersArray, correctAnswersArray));
+    }
+  }
+
+  console.log("All correct? ", !results.some((el) => el === false));
+}
 
 function submitEventHandler() {
-  console.log();
+  compareAnswers(answers.value, getCorrectAnswers());
 }
 </script>
 
